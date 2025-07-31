534872d2ea2c0b48b52fe6bb955b84e80e7efcf0
Antônio Miguel <antonio.miguel@estudante.ufjf>
2025-07-31 15:54:26 -0300

    cria discos

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const Tabuleiro = criaT();
 document.body.append(Tabuleiro);
 
-function criaT(){
-    const novoT = document.createElement("div");
-    novoT.classList.add("tabuleiro");
-    return novoT;
+function criaT() {
+  const novoT = document.createElement("div");
+  novoT.classList.add("tabuleiro");
+  return novoT;
+}
+
+for (let i = 0; i <= 6; i++) {
+  const Disco = criaD();
+  Tabuleiro.append(Disco);
+
+  if (i < 3) {
+    Disco.dataset.cor = "preto";
+  } else if (i > 3) {
+    Disco.dataset.cor = "branco";
+  } else {
+    Disco.dataset.cor = "transparente";
+  }
+}
+
+function criaD() {
+  const novoD = document.createElement("div");
+  novoD.classList.add("disco");
+  return novoD;
 }
